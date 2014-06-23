e7e19e6ea47ee58b9526fd5dedec1bedbf3eb71f
Josh Laughner <joshlaugh5@gmail.com>
2014-06-23 11:46:16 -0700

    Pre branch to rework boundary_layer_verification

--- plotting_scripts/findall_no2_bdy_layer_heights.m
+++ plotting_scripts/findall_no2_bdy_layer_heights.m
@@ -1,87 +1,71 @@
-function [ heights, median_times ] = findall_no2_bdy_layer_heights( data, data_sep, altitude, utc, utc_sep, field_name)
-%findall_bdy_layer_heights: Output boundary layer heights for a full day's
-%worth of data.
-%   This function will analyze a full day of aircraft data and find the
-%   median boundary height for clusters of boundary layer crossings.  This
-%   is based on Ashley Russell's method for comparing satellite data
-%   against boundary layer flights, described in her 2011 paper.
+function [ blh, utc ] = findall_no2_bdy_layer_heights(utc_in,no2,alt_no2,ranges_in)
+%findall_no2_bdy_layer_heights(utc, no2, ranges): Finds the boundary layer crossing for each range passed.
+%   This function finds the NO2 chemical boundary layer in aircraft data by
+%   examining the NO2 data during times when the aircraft is constantly
+%   ascending or descending.  These times are passed as the variable
+%   ranges, which must be an n x 2 matrix where each row is the start/stop
+%   time in seconds after midnight UTC for each range.  Several MAT files
+%   with these ranges stored can be found at ~/MATLAB/NO2
+%   Profiles/Workspaces
 %
-%   This function operates by identifying clusters of boundary layer
-%   crossings that occur in a relatively short period of time. (The utc_sep
-%   input determines the maximum allowed time between crossings that belong
-%   to the same cluster). The subset of data defined by this cluster is
-%   then passed to the "find_bdy_layer_heights" function, which calulates
-%   the boundary layer height for that cluster based on its criteria.
+%   utc_in and no2 are the values from the aircraft merge file.
 %
-%   This function requires 6 inputs:
-%       data: the measurements of the parameter for which you are finding
-%       the boundary layer, e.g. NO2 or potential temperature.
-%
-%       data_sep: The difference in value between two successive
-%       measurements that corresponds to a possible boundary layer entrance
-%       or exit. For NO2, ~1000 is a good number to start with.
-%
-%       altitude: the altitude of the plane in kilometers.  This must have
-%       a 1-to-1 correspondence with the data input.
-%
-%       utc: The times that correspond to both the altitude and the data
-%       input.  This can be any time data, although UTC is perhaps the most
-%       consistent.
-%
-%       utc_sep: The time gap that the function uses to separate clusters
-%       of boundary layer crossings.  1000 s is usually a good starting
-%       guess.
-%
-%       field_name: The name of the Merge structure field that "data" was
-%       read from.  This is necessary as the function
-%       "find_bdy_layer_height" (called below) uses difference criteria for
-%       different fields.
-%
-%   The values returned are the calculated median boundary layer heights
-%   and the times corresponding to those heights.
+%   This returns two values, the boundary layer heights and median UTC
+%   times of each range.
 
-yy = find(abs(diff(data)) > data_sep);
+% Remove any ranges that are entirely outside our UTC range
+range_start_test = ranges_in' < min(utc_in);
+range_end_test = ranges_in' > max(utc_in);
 
-% If we are looking at an NO2 field, reject the differences if
-%   a) The concurrent NO2 measurement is greater than 10,000 (this usually
-%   means the aircraft is passing through a plume from a city, forest fire,
-%   etc. that might confuse the boundary layer location)
-%   b) There is no consistent change in altitude as well
-if any(strcmp(field_name,{'NO2_LIF','NO2_UCB','NO2','NO2_MixingRatio_LIF'}));
-%     % Check if either value that contributed to the difference is greater
-%     % than 10,000 pptv
-%     tmp_no2 = [data(yy); data(yy+1)];
-%     tmp = tmp_no2 < 1e4;
-%     yy_high_no2 = false(1,size(tmp,2));
-%     for a = 1:size(tmp,2)
-%         yy_high_no2(a) = tmp(1,a) & tmp(2,a);
-%     end
-%     yy = yy(yy_high_no2);
+% As of Matlab v. R2013b, "all" acts on matrices (not vectors) by testing
+% if every element in a column (first index) is true. This line thus checks
+% if both the start and end of the range are before the lowest UTC value or
+% after the highest UTC value.
+out_of_range = all(range_start_test) | all(range_end_test);
+
+ranges = ranges_in(~out_of_range',:);
+
+% If no ranges are left, return empty matrices
+if isempty(ranges);
+    blh = [];
+    utc = [];
+else
     
-    % Check that the difference in altitude that corresponds to the change
-    % in NO2 is greater than 20 m/s
-    dz_dt = diff(data)./diff(utc);
-    yy = yy(dz_dt(yy) > 0.02);
-end
-
-zz = find(abs(diff(utc(yy+1))) > utc_sep);
-n = numel(zz) - 1;
-
-heights = -9e9*ones(1,n);
-median_times = -9e9*ones(1,n);
-
-for a=1:n
-    %if any(strcmp(field_name,{'NO2_LIF','NO2_UCB','NO2'})) && any(data(yy(zz(a))+1:yy(zz(a+1)))>=1e4)
-    %else
-    blh = find_bdy_layer_height(data(yy(zz(a))+1:yy(zz(a+1))),altitude(yy(zz(a))+1:yy(zz(a+1))), field_name);
-    t = median(utc(yy(zz(a))+1:yy(zz(a+1))));
+    s=size(ranges);
+    blh = -99*ones(s(1),1);
+    utc = -99*ones(s(1),1);
     
-    heights(a) = blh;
-    median_times(a) = t;
-    %end
+    for a=1:s(1)
+        startind = find(abs(utc_in - ranges(a,1)) == min(abs(utc_in - ranges(a,1))));
+        endind = find(abs(utc_in - ranges(a,2)) == min(abs(utc_in - ranges(a,2))));
+        no2seg = no2(startind:endind);
+        altseg = alt_no2(startind:endind);
+        utc(a) = nanmedian(utc_in(startind:endind));
+        
+        % Bin the data so that the vertical profiles are less noisy
+        [no2seg, altseg] = bin_vertical_profile(altseg,no2seg,0.5);
+        
+%         % For most measurements, to find the boundary layer, we look for the
+%         % altitude at which the [NO2] is 1/e of its maximum value (as well as
+%         % requiring that d[NO2]/dz < 0).  However, when 1 e-fold of the max
+%         % value is < 40 pptv, this method tends to run into problems because
+%         % the average [NO2] above the BL is ~40 pptv. Thus, when this is the
+%         % case, we look for the greatest gradient in NO2, with d[NO2]/dz < 0
+%         if max(no2seg(:))*exp(-1) < 40;
+%             method = 'max';
+%         else
+%             method = 'exp';
+%         end
+        method = 'max'; % 23 Jun 2014: We're going to try just looking for the maximum gradient to see if that brings the column densities down
+        blh(a) = find_bdy_layer_height(no2seg, altseg,method);
+    end
+    
+    % Clean up the output: if any fill values (-99) are left, remove those
+    % entries
+    
+    fills = blh == -99;
+    blh = blh(~fills);
+    utc = utc(~fills);
 end
-
-heights(heights < -1e9) = []; median_times(median_times < -1e9) = [];
-
 end
 
--- satellite_verification_scripts/boundary_layer_verification.m
+++ satellite_verification_scripts/boundary_layer_verification.m
@@ -1,4 +1,4 @@
-function [no2,alt,utc,interp_height] = boundary_layer_verification( Merge, Data, tz, varargin )
+function [omi_lon, omi_lat, OMI_NO2, BEHR_NO2, DC8_NO2] = boundary_layer_verification( Merge, Data, tz, varargin )
 %[ pix_lat, pix_lon, BEHR_NO2, aircraft_NO2, OMI_NO2 ]
 
 %boundary_layer_verification: Compare satellite and aircraft measurements
@@ -17,21 +17,14 @@ function [no2,alt,utc,interp_height] = boundary_layer_verification( Merge, Data,
 %       PST = Pacific Std.      
 %
 %   Since Aura satellite overpass is ~1:45 p in standard time, do not use
-%   daylight savings time values.
+%   daylight savings time values.  Further, you must pass only one element
+%   of Data; the one corresponding to the swath of interest.
 %
 % Parameters
 %   timerange: By default, this method will restrict flight data to between
 %   12:00p and 3:00p local, as per Russell et. al.  This can be overridden
 %   using this parameter, which requires a cell array with the start and
 %   end times as strings in military time format.
-%
-%   NO2_sep: This is the minimum difference between adjacent NO2
-%   measurements to qualify as a boundary layer entrance exit.  It defaults
-%   to 1000 pptv.
-%
-%   time_sep: The maximum time (in seconds) between "clusters" of boundary
-%   layer entrances and exits - see "utc_sep" in
-%   findall_no2_bdy_layer_heights.m for more information. Defaults to 1000 s.
 %
 %   cloud_product: Which cloud product (omi or modis) to use in rejecting
 %   pixels.  Defaults to omi.
@@ -43,38 +36,55 @@ function [no2,alt,utc,interp_height] = boundary_layer_verification( Merge, Data,
 %   Defaults to 'AlwaysByRow'.  See omi_pixel_reject or omi_rowanomaly for
 %   more information on the possible choices ('AlwaysByRow', 'RowsByTime',
 %   'XTrackFlags', and 'XTrackFlagsLight').
+%
+%   DEBUG_LEVEL: The level of output messages to write; 0 = none, 1 =
+%   normal; 2 = verbose
 
 p = inputParser;
 p.addRequired('Merge',@isstruct);
 p.addRequired('Data',@isstruct);
 p.addRequired('timezone', @(x) any(strcmpi(x,{'est','cst','mst','pst',})));
 p.addParamValue('timerange',{'12:00','15:00'},@iscell)
-p.addParamValue('NO2_sep',1e3,@isscalar);
-p.addParamValue('time_sep',1e3,@isscalar);
 p.addParamValue('cloud_product','omi',@(x) any(strcmpi(x,{'omi','modis'})));
 p.addParamValue('cloud_frac_max',0.2, @isscalar);
 p.addParamValue('rowanomaly','AlwaysByRow',@(x) strcmp(x,{'AlwaysByRow','RowsByTime','XTrackFlags','XTrackFlagsLight'}));
+p.addParamValue('DEBUG_LEVEL',1,@isscalar);
 
 p.parse(Merge,Data,tz,varargin{:});
 pout = p.Results;
+
+% Check that only one element of Data was passed
+if numel(Data)>1; error('bdy_layer_verify:DataInput','Only pass one top-level element of the Data structure'); end
 
 Merge = pout.Merge;
 Data = pout.Data;
 tz = pout.timezone;
 timerange = pout.timerange;
-NO2_sep = pout.NO2_sep;
-time_sep = pout.time_sep;
 cloud_prod = pout.cloud_product;
 cloud_frac_max = pout.cloud_frac_max;
 rowanomaly = pout.rowanomaly;
+DEBUG_LEVEL = pout.DEBUG_LEVEL;
 
 % Load the aircraft data
-no2 = Merge.Data.NO2_UCB.Values;
-alt = Merge.Data.ALTP.Values;
-utc = Merge.Data.UTC.Values;
-lat = Merge.Data.LATITUDE.Values;
-lon = Merge.Data.LONGITUDE.Values;
+if DEBUG_LEVEL > 0; fprintf(' Getting data and restricting to 12:00-15:00 %s\n',tz); end
+no2_raw = Merge.Data.NO2_UCB.Values;
+alt_raw = Merge.Data.ALTP.Values;
+radar_alt_raw = Merge.Data.Radar_Altitude.Values;
+utc_raw = Merge.Data.UTC.Values;
+lat_raw = Merge.Data.LATITUDE.Values;
+lon_raw = Merge.Data.LONGITUDE.Values - 360;
+pressure_raw = Merge.Data.PRESSURE.Values;
+temperature_raw = Merge.Data.TEMPERATURE.Values;
 
+% Get the date
+merge_date = Merge.metadata.date;
+
+% Load the file containing the altitude ranges in which to look for
+% boundary layer crossings. Loads the variable "Ranges," a structure.
+
+load('/Users/Josh/Documents/MATLAB/NO2 Profiles/Workspaces/ARCTAS-CA Altitude Ranges Exclusive.mat');
+% Store the date corresponding to each index for easy searching later
+range_dates = {datestr(Ranges(1).Date,29), datestr(Ranges(2).Date,29), datestr(Ranges(3).Date,29), datestr(Ranges(4).Date,29)};
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  RESTRICT TO TIME RANGE  %%%%%
@@ -83,43 +93,66 @@ lon = Merge.Data.LONGITUDE.Values;
 % Remove any values outside the time range (12:00-3:00p local standard by
 % default)
 utcstart = local2utc(timerange{1}, tz); utcend = local2utc(timerange{2}, tz);
-time_logical = utc >= utcstart & utc <= utcend;
-no2 = no2(time_logical);
-alt = alt(time_logical);
-utc = utc(time_logical);
-lat = lat(time_logical);
-lon = lon(time_logical);
+time_logical = utc_raw >= utcstart & utc_raw <= utcend;
+no2 = no2_raw(time_logical);
+alt = alt_raw(time_logical);
+radar_alt = radar_alt_raw(time_logical);
+utc = utc_raw(time_logical);
+lat = lat_raw(time_logical);
+lon = lon_raw(time_logical);
+pressure = pressure_raw(time_logical);
 
-% Find any fill, ULOD, or LLOD values in any of the imported data sets and
-% remove them.
+% We do not want to restrict temperature, as it will be used to get a
+% temperature profile for conversion of 
+
+% Find any fill, ULOD, or LLOD values in any of the imported data sets relating to NO2
+% and remove them.
 no2_fill = Merge.Data.NO2_UCB.Fill;
 alt_fill = Merge.Data.ALTP.Fill;
+ralt_fill = Merge.Data.Radar_Altitude.Fill;
 lat_fill = Merge.Data.LATITUDE.Fill;
 lon_fill = Merge.Data.LONGITUDE.Fill;
+pres_fill = Merge.Data.PRESSURE.Fill;
 
-fill_logical = no2 ~= no2_fill & alt ~= alt_fill & lat ~= lat_fill & lon ~= lon_fill;
+fill_logical = no2 ~= no2_fill & alt ~= alt_fill & lat ~= lat_fill & lon ~= lon_fill & pressure ~= pres_fill & radar_alt == ralt_fill;
 
 ulod = Merge.metadata.upper_lod_flag;
 llod = Merge.metadata.lower_lod_flag;
 
-lod_logical = no2 ~= ulod & no2 ~= llod & alt ~= ulod & alt ~= llod; % Lat, lon, and utc should not be subject to limits of detection
+lod_logical = no2 ~= ulod & no2 ~= llod & alt ~= ulod & alt ~= llod & pressure ~= ulod & pressure ~= llod; % Lat, lon, radar altitude, and utc should not be subject to limits of detection
 
 no2 = no2(lod_logical & fill_logical);
 alt = alt(lod_logical & fill_logical);
+radar_alt = radar_alt(lod_logical & fill_logical);
 utc = utc(lod_logical & fill_logical);
 lat = lat(lod_logical & fill_logical);
 lon = lon(lod_logical & fill_logical);
+pressure = pressure(lod_logical & fill_logical);
+
+% Now for temperature
+temperature_fill = Merge.Data.TEMPERATURE.Fill;
+T_fill_logical = temperature_raw ~= temperature_fill & alt_raw ~= alt_fill;
+T_lod_logical = temperature_raw ~= ulod & temperature_raw ~= llod & alt_raw ~= ulod & alt_raw ~= llod;
+temperature = temperature_raw(T_fill_logical & T_lod_logical);
+alt_T = alt_raw(T_fill_logical & T_lod_logical);
+utc_T = utc_raw(T_fill_logical & T_lod_logical);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  FIND BDY LAYER HEIGHTS  %%%%%  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if DEBUG_LEVEL > 0; fprintf(' Calculating & interpolating boundary layer heights.\n'); end
 
-[heights, times] = findall_no2_bdy_layer_heights(no2, NO2_sep, alt, utc, time_sep, 'NO2');
-if isempty(heights)
-    error('bdy_layer_verify:findBoundaryLayer','Could not find boundary layer heights.')
+% Get the range for today;
+d = find(datenum(range_dates) == datenum(merge_date));
+
+[heights, times] = findall_no2_bdy_layer_heights(utc_raw, no2_raw, alt_raw, Ranges(d).Ranges);
+nans = isnan(heights);
+heights = heights(~nans); times = times(~nans);
+if numel(heights) < 2
+    error('bdy_layer_verify:findBoundaryLayer','Could not find 2 boundary layer heights - needed for interpolation')
 end
 
-interp_height = interp1(times, heights, utc); % Linearly interpolate the boundary layer height to every value of UTC.
+interp_height = interp1(times, heights, utc_raw); % Linearly interpolate the boundary layer height to every value of UTC.
 
 % For values outside of the range of "times," assume that the boundary
 % layer height equals the closest value.
@@ -133,9 +166,188 @@ if last_height < numel(interp_height)
     interp_height(last_height+1:end) = interp_height(last_height);
 end
 
+% Now clip the interpolated height to the time range and clear values
+% corresponding to NO2 fills/lod flags
+interp_height = interp_height(time_logical);
+interp_height = interp_height(lod_logical & fill_logical);
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%  CALCULATE NO2 COLUMNS   %%%%%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Integrate aircraft NO2 measurements from the boundary layer height to the
+% surface (i.e. over the distance defined by radar altitude).  If the plane
+% is above the boundary layer, set column to NaN since no data is available
+% on BL NO2.
+if DEBUG_LEVEL > 0; fprintf(' Integrating columns\n'); end
 
+% First, extract a temperature profile; we will need this to convert mixing
+% ratios to number densities
+[T_bins, T_bin_alt] = bin_vertical_profile(alt_T, temperature, 0.3);
+% The temperature profiles often have an inversion in the BL, so split the
+% temperature data into slope <0 (remnant PBL/free troposphere) and >=0
+% (mixed layer)
+ML_ind = find(diff(T_bins) < 0,1,'first')+1; % The +1 accounts for the fact that length(diff(x)) is length(x) - 1
+[T_poly_mixed, T_R2_mixed] = polyfit_R2(T_bin_alt(1:ML_ind),T_bins(1:ML_ind),1);
+[T_poly_free, T_R2_free] = polyfit_R2(T_bin_alt(ML_ind:end),T_bins(ML_ind:end),1);
+% We'll calculate the mixed layer height as the intersection of the two
+% fits
+A = [-T_poly_free(1), 1; -T_poly_mixed(1), 1]; b = [T_poly_free(2); T_poly_mixed(2)];
+X = A\b; ML_height = X(1)*1000;
+
+% We need the P0 and scale height (H) used in the conversion of pressure to
+% altitude.  Fit the data to a line such that ln(P) = (-1/H)z + ln(P0),
+% thus H = -1/slope and P0 = e^(intercept)
+P = polyfit(alt_raw,log(pressure_raw),1);
+P0 = exp(P(2)); H = -1/P(1);
+
+% Also, define Avogadro's number and gas constant;
+Av = 6.022e23; % molec. / mol
+R = 8.314e4; % (hPa * cm^3) / (mol * K)
+
+% Get the GLOBE terrain altitude for this region
+latminmax = [floor(min(lat)), ceil(max(lat))];
+lonminmax = [floor(min(lon)), ceil(max(lon))];
+globe_dir = '/Volumes/share/GROUP/SAT/BEHR/GLOBE_files';
+[terpres, refvec] = globedem(globe_dir,1,latminmax,lonminmax);
+cell_count = refvec(1);
+globe_latmax = refvec(2); globe_latmin = globe_latmax - size(terpres,1)*(1/cell_count);
+globe_lat_matrix = (globe_latmin + 1/(2*cell_count)):(1/cell_count):globe_latmax;
+globe_lat_matrix = globe_lat_matrix';
+globe_lat_matrix = repmat(globe_lat_matrix,1,size(terpres,2));
+
+globe_lonmin = refvec(3); globe_lonmax = globe_lonmin + size(terpres,2)*(1/cell_count);
+globe_lon_matrix = globe_lonmin + 1/(2*cell_count):(1/cell_count):globe_lonmax;
+globe_lon_matrix = repmat(globe_lon_matrix,size(terpres,1),1); 
+
+terpres(isnan(terpres)) = 0; % The Globe database fills oceans with NANs.  Since we care about column height, we want oceans to be at 0 altitude.
+
+% Prepare variables from the BEHR Data Structure
+omi_lat = Data.Latitude; omi_lon = Data.Longitude;
+TP_pres = Data.TropopausePressure;
+
+% Now numerically integrate the columns.
+no2_integrated_col = -127*ones(1,numel(no2));
+dz = 1; % width of integration step in meters
+for c = 1:numel(no2)
+    
+    if alt(c) > interp_height(c);
+        if DEBUG_LEVEL > 1; fprintf('\t Column %d of %d - above BL\n',c,numel(no2)); end
+        no2_column = NaN; %Skip any columns where the plane is above the BL
+    elseif no2(c) < 0;
+        no2_column = NaN; %Also skip any columns where the NO2 measurement is negative
+    else
+        if DEBUG_LEVEL > 1; fprintf('\t Column %d of %d\n',c,numel(no2)); end
+        % We'll integrate in groups of 1 m (100 cm)
+        no2_column = 0;
+        BLH = round(interp_height(c)*1000); %Get the boundary layer height in meters
+        
+        % Find the OMI pixel that corresponds to this measurement, get the
+        % tropopause altitude
+        dlat = abs(omi_lat - lat(c)); dlon = abs(omi_lon - lon(c));
+        dtot = dlat + dlon; xx = (dtot == min(dtot(:)));
+        TP_alt = round(log(TP_pres(xx)/P0)*(-H)*1000); % Converted to meters
+        
+        %Radar altitude is the distance from the plane to surface, pressure
+        %altitude is the planes "absolute" altitude, therefore the surface
+        %altitude should be the difference.  Convert to meters.
+%        surface_alt = round((alt(c) - radar_alt(c))*1000); 
+        
+        % Since the radar was apparently broken on all four ARCTAS flights,
+        % we'll instead find the closest GLOBE measurement and use that for
+        % surface altitude
+        
+        dlat = abs(globe_lat_matrix - lat(c)); dlon = abs(globe_lon_matrix - lon(c));
+        dtot = dlat + dlon; xx = (dtot == min(dtot(:)));
+        surface_alt = round(terpres(xx)); % Globe measurements should be in meters above sea level
+        
+        if surface_alt < BLH
+            % Surface to boundary layer
+            for h=surface_alt:dz:BLH
+                P = P0 * exp(-h/(H*1000));
+                if h <= ML_height; % Account for the temperature inversion in the mixed layer
+                    %T = polyval(T_poly_mixed,h/1000);
+                    T = T_poly_mixed(1)*(h/1000) + T_poly_mixed(2);
+                elseif h > ML_height;
+                    %T = polyval(T_poly_free,h/1000);
+                    T = T_poly_free(1)*(h/1000) + T_poly_free(2);
+                end
+                conc_NO2 = (Av * P * no2(c)*1e-12)/(R * T); % molec./cm^3, mixing ratio of NO2 is in pptv
+                no2_column = no2_column + conc_NO2 * dz * 100; % Integrating in 100dz cm increments
+            end
+            % Boundary layer to tropopause
+            for h=(BLH+1):dz:TP_alt
+                P = P0 * exp(-h/(H*1000));
+                %T = polyval(T_poly_free,h/1000);
+                T = T_poly_free(1)*(h/1000) + T_poly_free(2);
+                conc_NO2 = (Av * P * 40*1e-12)/(R * T); % molec./cm^3, mixing ratio of NO2 is in pptv.  Assume 40 pptv in the free troposphere
+                no2_column = no2_column + conc_NO2 * dz * 100; % Integrating in 100dz cm increments
+            end
+        else
+            % Surface to tropopause
+            for h=surface_alt:dz:TP_alt
+                P = P0 * exp(-h/(H*1000));
+                %T = polyval(T_poly_free,h/1000);
+                T = T_poly_free(1)*(h/1000) + T_poly_free(2);
+                conc_NO2 = (Av * P * 40*1e-12)/(R * T); % molec./cm^3, mixing ratio of NO2 is in pptv.  Assume 40 pptv in the free troposphere
+                no2_column = no2_column + conc_NO2 * dz * 100; % Integrating in 100dz cm increments
+            end
+        end
+    end
+    no2_integrated_col(c) = no2_column;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% AVERAGE COLUMNS TO PIXELS  %%%%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if DEBUG_LEVEL > 0; fprintf(' Averaging columns to pixels\n'); end
+
+% Find pixels that fail the criteria used for BEHR mapping.
+% omi_pixel_reject.m works by setting Areaweight to 0 for any pixels that
+% fail the criteria. Hence, we need to create an areaweight field that
+% assumes all pixels are valid.
+
+Data.Areaweight = ones(size(Data.Longitude));
+if DEBUG_LEVEL > 0; fprintf('   Rejecting with omi_pixel_reject.m\n'); end
+Data2 = omi_pixel_reject(Data,cloud_prod,cloud_frac_max,rowanomaly);
+xx = Data2.Areaweight > 0;
+
+% Read in corner lat/lon and BEHR NO2 columns. Latcorn/loncorn are 4 x n
+% fields, where the first dimension is each corner for a pixel.
+omi_lat = Data2.Latitude(xx); omi_lon = Data2.Longitude(xx);
+corner_lat = Data2.Latcorn(:,xx); corner_lon = Data2.Loncorn(:,xx);
+BEHR_NO2 = Data2.BEHRColumnAmountNO2Trop(xx);
+OMI_NO2 = Data2.ColumnAmountNO2Trop(xx);
+
+% Remove from consideration all pixels whose corners fall entirely outside
+% the range of lat/lons that the aircraft flew
+if DEBUG_LEVEL > 0; fprintf('   Removing pixels outside aircraft track\n'); end
+lon_logical = corner_lon > min(lon) & corner_lon < max(lon);
+lat_logical = corner_lat > min(lat) & corner_lat < max(lat);
+latlon_logical = any(lon_logical) & any(lat_logical); % any() operates on matrices along the first dimension; here that is each corner for pixel n
+
+omi_lat = omi_lat(latlon_logical);
+omi_lon = omi_lon(latlon_logical);
+corner_lat = corner_lat(:,latlon_logical);
+corner_lon = corner_lon(:,latlon_logical);
+BEHR_NO2 = BEHR_NO2(latlon_logical);
+OMI_NO2 = OMI_NO2(latlon_logical);
+
+% For each pixel that remains, average the aircraft columns to each pixel
+if DEBUG_LEVEL > 0; fprintf('   Averaging columns\n'); end
+DC8_NO2 = NaN(size(BEHR_NO2));
+for a=1:numel(BEHR_NO2)
+    if DEBUG_LEVEL > 1; fprintf('   Pixel %d of %d\n',a,numel(BEHR_NO2)); end
+    IN = inpolygon(lon,lat,corner_lon(:,a),corner_lat(:,a));
+    DC8_NO2(a) = nanmean(no2_integrated_col(IN));
+end
+
+% Finally, remove pixels that didn't have any aircraft measurements
+notnans = ~isnan(DC8_NO2);
+omi_lat = omi_lat(notnans); omi_lon = omi_lon(notnans);
+BEHR_NO2 = BEHR_NO2(notnans); OMI_NO2 = OMI_NO2(notnans);
+DC8_NO2 = DC8_NO2(notnans);
 end
 
