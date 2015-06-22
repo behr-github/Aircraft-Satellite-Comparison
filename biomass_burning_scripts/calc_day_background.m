05bfd06e26acba8b648cfc84380725eb21491118
Josh Laughner <Josh@dhcp-128-32-208-224.lips.berkeley.edu>
2015-06-22 12:28:58 -0700

    Updates to utility functions calc_day_background and remove_merge_fills
    MODIFIED FILES:
        biomass_burning_scripts/calc_day_background - now can accept the
    species name as either one defined in merge_field_names or a field in
    Merge.Data (will try merge_field_names first)
    
        utility_scripts/remove_merge_fields - now allows for the "Fill"
    field to be the string "n/a" (case insensitive), which means that
    asking it to get UTC does not throw an error. Also handles specially
    if the field asked for is "LONGITUDE" (which needs to have all values
    180 converted to the proper value).  This was already being done with
    the fourth-output lon, but now will be done if longitude is asked for
    explicitly (makes it easier if longitude is all we want, we don't have
    to remember which number output it was). Consequently the code to
    "fix" longitude values was put in a subfunction.

--- biomass_burning_scripts/calc_day_background.m
+++ biomass_burning_scripts/calc_day_background.m
@@ -10,15 +10,19 @@ E=JLLErrors;
 
 Names = merge_field_names(campaign_name);
 
-if ~isfield(Names, species_name)
-    E.badinput('%s is not a defined species for %s', species_name, campaign_name)
+if isfield(Names, species_name)
+    merge_datafield = Names.(species_name);
+elseif isfield(Merge.Data, species_name);
+    merge_datafield = species_name;
+else
+    E.badinput('%s is neither defined in merge_field_names nor exists in Merge.Data for %s', species_name, campaign_name);
 end
 
 if nargin < 4
     bottom_percent = 0.1;
 end
 
-species = remove_merge_fills(Merge, Names.(species_name));
+species = remove_merge_fills(Merge, merge_datafield);
 species(isnan(species)) = [];
 
 species_background = quantile(species, bottom_percent);
